76328d7e8cbf853cdfebdd02a12b28efeda24aa8
Ashish <ashishsaud498@gmail.com>
2025-02-06 23:19:18 +0545

    create action

--- src/features/UserDetailSice.ts
+++ src/features/UserDetailSice.ts
@@ -1,5 +1,30 @@
 import { createSlice , createAsyncThunk} from "@reduxjs/toolkit";
 
+//create action
+export const createUser = createAsyncThunk(
+  "createUser",
+  async (data, { rejectWithValue }) => {
+    console.log("data", data);
+    const response = await fetch(
+      "https://67a23a30409de5ed5254bc5d.mockapi.io/ashishcrud",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+
+    try {
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
 export const userDetailSlice = createSlice({
   name: "userDetails",
   initialState: {
